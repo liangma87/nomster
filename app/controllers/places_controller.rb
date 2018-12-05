468bab96410e51a4247f3cbc3273843577ab6ddf
liang <mliang3176@gmail.com>
2018-12-05 06:13:48 +0000

    Added update & destory function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,6 +25,18 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
