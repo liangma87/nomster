51f5fa335392b4f7bd88e461b12d4098c7e88ea5
liang <mliang3176@gmail.com>
2018-12-11 05:31:09 +0000

    Add geocode support

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,6 @@ class Place < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true
   validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
