5288efc1334dc90db78331fda983f5ec507e6d58
liang <mliang3176@gmail.com>
2018-12-26 01:04:32 +0000

    fixed the s3 permission issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    region:                ENV['AWS_REGION'],            # required
+    region:                ENV['AWS_REGION'],            # optional
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],        # required
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV['AWS_BUCKET']              # required
+  config.fog_public     = false                          # optional, defaults to true
 end
